45530bf5d90ead4e9063cf7d9efae507fd536b0d
Drew Pereli <drewpereli@gmail.com>
2022-10-13 16:12:03 -0700

    Add some more guns

--- src/entities/gun.ts
+++ src/entities/gun.ts
@@ -10,9 +10,25 @@ export class ShotGun extends Gun {
   damage = 10;
   spread = 20;
   range = 10;
+  attackTimeMultiplier = 4;
 }
 
 export class Pistol extends Gun {
   damage = 5;
   range = 5;
+  attackTimeMultiplier = 4;
+}
+
+export class AssaultRifle extends Gun {
+  damage = 20;
+  range = 20;
+  penetration = 4;
+  attackTimeMultiplier = 10;
+}
+
+export class SubMachineGun extends Gun {
+  damage = 5;
+  range = 10;
+  penetration = 0;
+  attackTimeMultiplier = 1;
 }
