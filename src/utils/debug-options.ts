5f84f2eaac65c56b20f42851d324c4ffbedb743b
Drew Pereli <drewpereli@gmail.com>
2022-10-17 22:42:27 -0700

    Add fullViewAngle debug option

--- src/utils/debug-options.ts
+++ src/utils/debug-options.ts
@@ -7,8 +7,9 @@ const activeOptions = {
   randomHalfWallsInEmptyMap: 10,
   randomLavaInEmptyMap: 10,
   docileEnemies: false,
-  extraEnemies: 0,
-  infiniteHealth: true,
+  extraEnemies: 1,
+  infiniteHealth: 0,
+  fullViewAngle: true,
 };
 
 export const debugOptions = debugMode
