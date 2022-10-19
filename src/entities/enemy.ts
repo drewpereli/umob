e131ef6cb4b6f9327a6ffd9dd919fdc28a19b38c
Drew Pereli <drewpereli@gmail.com>
2022-10-18 18:31:25 -0700

    Slight refactoring
    Move some _act functionality into its own function

--- src/entities/enemy.ts
+++ src/entities/enemy.ts
@@ -23,29 +23,39 @@ export class Enemy extends NonPlayerActor {
       if (this.canAttackPlayer) return this.fireWeapon([this.game.player]);
 
       if (this.canSeePlayer || this.lastSawPlayerAt) {
-        const targetCoords = this.canSeePlayer
-          ? this.game.player.coords
-          : (this.lastSawPlayerAt as Coords);
-
-        const coordsPathToPlayer = this.game.map.pathBetween(
-          this.coords,
-          targetCoords,
-          this
-        );
-
-        const coordsTowardsPlayer = coordsPathToPlayer[0];
-
-        if (!coordsTowardsPlayer) return;
-
-        const tile = this.game.map.tileAt(coordsTowardsPlayer);
-
-        if (!this.canMoveTo(tile)) return;
-
-        this.moveOrTurn(tile);
+        this._moveTowardsPlayerOrLastSeen();
       } else {
         this.wander();
       }
     }
+  }
+
+  _moveTowardsPlayerOrLastSeen() {
+    const targetCoords = this.canSeePlayer
+      ? this.game.player.coords
+      : this.lastSawPlayerAt;
+
+    if (!targetCoords) {
+      throw new Error(
+        `Called _moveTowardsPlayerOrLastSeen when cant see player and no lastSawPlayerAt`
+      );
+    }
+
+    const coordsPathToPlayer = this.game.map.pathBetween(
+      this.coords,
+      targetCoords,
+      this
+    );
+
+    const coordsTowardsPlayer = coordsPathToPlayer[0];
+
+    if (!coordsTowardsPlayer) return;
+
+    const tile = this.game.map.tileAt(coordsTowardsPlayer);
+
+    if (!this.canMoveTo(tile)) return;
+
+    this.moveOrTurn(tile);
   }
 
   updatePosition(coords: Coords) {
