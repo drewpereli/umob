d62c67f903278c30ba0510e6c2f412761418cde6
Drew Pereli <drewpereli@gmail.com>
2022-10-19 17:18:00 -0700

    Add clip status to player status window

---
+++ src/components/WeaponClipStatus.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue';
+import type Gun from '@/entities/gun';
+
+export default defineComponent({
+  props: {
+    gun: {
+      type: Object as PropType<Gun>,
+      required: true,
+    },
+  },
+  computed: {
+    clipSize() {
+      return this.gun.clipSize;
+    },
+    amoLoaded() {
+      return this.gun.amoLoaded;
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="weapon-clip-status">
+    <span
+      v-for="n in clipSize"
+      :key="n"
+      class="amo-slot"
+      :class="{ loaded: n <= amoLoaded }"
+    />
+  </div>
+</template>
+
+<style scoped lang="stylus">
+.weapon-clip-status
+  display flex
+  flex-wrap wrap
+  gap 0.25rem
+
+.amo-slot
+  width 1rem
+  height 0.5rem
+  background-color #000
+  border 1px solid #888
+  border-radius 2px
+
+  &.loaded
+    background-color green
+</style>
