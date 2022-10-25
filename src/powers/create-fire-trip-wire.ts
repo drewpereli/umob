82c0bae0e92a3d8ae88a3cd3c319ed4b0e05abb7
Drew Pereli <drewpereli@gmail.com>
2022-10-25 00:34:32 -0700

    Add fire trip wire

---
+++ src/powers/create-fire-trip-wire.ts
@@ -0,0 +1,17 @@
+import { CreateTripWire } from './create-trip-wire';
+import { FireTripWire } from '@/entities/traps/fire-tripwire';
+
+export class CreateFireTripWire extends CreateTripWire {
+  activate() {
+    const closest = this.closestValidToSelected();
+
+    if (!closest) return;
+
+    const tile = this.game.map.tileAt(closest);
+
+    const mine = new FireTripWire(tile, this.currentOrientation);
+    this.game.addMapEntity(mine);
+
+    return true;
+  }
+}
