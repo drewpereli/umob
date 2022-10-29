33e2db0664bf1f9d92768718faa052c79d74892b
Drew Pereli <drewpereli@gmail.com>
2022-10-28 17:47:21 -0700

    Add push power

---
+++ src/powers/push.ts
@@ -0,0 +1,24 @@
+import { TURN } from '@/stores/game';
+import type { Tile } from '@/stores/map';
+import { TargetedPower } from './targeted-power';
+
+export class Push extends TargetedPower {
+  name = 'push';
+  canTargetMovementBlocker = true;
+  energyCost = 20;
+  useTime = TURN;
+  range = 7;
+
+  get canActivate() {
+    const closest = this.closestValidToSelected() as Tile;
+    return super.canActivate && closest.creatures.length > 0;
+  }
+
+  activate() {
+    const tile = this.closestValidToSelected() as Tile;
+
+    tile.creatures.forEach((creature) => {
+      creature.receiveKnockBack(0, 7, this.owner);
+    });
+  }
+}
