e57eff416b4ddf741a691476059ea0ccb1ff5b76
Drew Pereli <drewpereli@gmail.com>
2022-11-02 12:27:34 -0700

    Add generic "add status effect to self" power

---
+++ src/powers/add-status-effect-to-self.ts
@@ -0,0 +1,48 @@
+import type Creature from '@/entities/creatures/creature';
+import { OcclusionVisualizer } from '@/status-effects/occlusion-visualizer';
+import type { StatusEffect } from '@/status-effects/status-effect';
+import { TargetingArray } from '@/status-effects/targeting-array';
+import { TURN } from '@/stores/game';
+import { NonTargetedPower } from './non-targeted-power';
+
+export abstract class AddStatusEffectToSelf extends NonTargetedPower {
+  // static powerName = statusEffect.name;
+  // static description = statusEffect.description?;
+
+  abstract statusEffect: new (
+    creature: Creature,
+    maxDuration: number
+  ) => StatusEffect;
+
+  useTime = TURN;
+  coolDown = 40 * TURN;
+  statusEffectMaxDuration = 20 * TURN;
+
+  activate() {
+    const effect = new this.statusEffect(
+      this.owner,
+      this.statusEffectMaxDuration
+    );
+
+    this.owner.addStatusEffect(effect);
+
+    return true;
+  }
+}
+
+export class ActivateOcclusionVisualizer extends AddStatusEffectToSelf {
+  static powerName = 'activate occlusion visualizer';
+  static description =
+    'Activates a network of nanobots that feeds you a 360° view of everything around you.';
+
+  statusEffect = OcclusionVisualizer;
+}
+
+export class ActivateTargetingArray extends AddStatusEffectToSelf {
+  static powerName = 'activate targeting array';
+  static description =
+    'Activate a targeting array that greatly raises your accuracy while active';
+  useTime = 0;
+  coolDown = 50 * TURN;
+  statusEffect = TargetingArray;
+}
