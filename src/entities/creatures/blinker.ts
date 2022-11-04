630b8a3798e5b5a30bf47ae39dee12f4ae6e97ce
Drew Pereli <drewpereli@gmail.com>
2022-11-03 17:03:57 -0700

    Add blinkers

---
+++ src/entities/creatures/blinker.ts
@@ -0,0 +1,69 @@
+import { Blink } from '@/powers/blink';
+import { useMap } from '@/stores/map';
+import type { Tile } from '@/tile';
+import { tap } from '@/utils/general';
+import { distance, rotateDir } from '@/utils/map';
+import { Scalpel } from '../weapons/melee-weapon';
+import Creature, { AiState, CreatureAlignment } from './creature';
+
+const weapon = new Scalpel();
+
+export class Blinker extends Creature {
+  constructor(tile: Tile, alignment?: CreatureAlignment) {
+    super(tile, alignment);
+
+    const defaultEngagingAction = this._aiStateActions[AiState.Engaging];
+
+    this._aiStateActions = {
+      ...this._aiStateActions,
+      [AiState.Engaging]: () => {
+        if (this.maybeBlink()) return;
+        defaultEngagingAction();
+      },
+    };
+  }
+
+  mass = 25;
+  name = 'blinker';
+  defaultChar = 'b';
+  color = 'white';
+
+  maxHealth = 50;
+  _health = 50;
+
+  equippedWeapon = weapon;
+  inventory = [weapon];
+
+  powers: [Blink] = [tap(new Blink(this), (p) => (p.currentUpgradeLevel = 2))];
+
+  maybeBlink(): boolean {
+    const blink = this.powers[0];
+
+    if (!blink.canActivate) return false;
+
+    const enemy = this.enemiesSeen[0];
+
+    if (!enemy) return false;
+
+    // If next to enemy and behind it, don't do anything, that's where we want to be
+    const tileBehindEnemy = tileBehind(enemy);
+    if (!tileBehindEnemy || tileBehindEnemy.id === this.tile.id) return false;
+
+    if (!this.game.creatureCanOccupy(tileBehindEnemy)) return false;
+
+    const distanceTo = distance(this, tileBehindEnemy);
+
+    if (distanceTo > blink.range) return false;
+
+    blink.activate(tileBehindEnemy);
+
+    return true;
+  }
+}
+
+function tileBehind(creature: Creature): Tile | undefined {
+  const facing = creature.facing;
+  const dir = rotateDir(facing, 2);
+
+  return useMap().adjacentTile(creature.tile, dir);
+}
--- src/entities/weapons/melee-weapon.ts
+++ src/entities/weapons/melee-weapon.ts
@@ -1,4 +1,4 @@
-import { Weapon } from './weapon';
+import { Weapon, DEFAULT_FLANKING_BONUS } from './weapon';
 
 export default abstract class MeleeWeapon extends Weapon {
   char = '}';
@@ -12,4 +12,11 @@ export class Pipe extends MeleeWeapon {
   damage = 20;
   attackTimeMultiplier = 2;
   description = "It's a pipe";
+}
+
+export class Scalpel extends MeleeWeapon {
+  name = 'scalpel';
+  description = 'A sharp scalpel. Flanking bonus is doubled.';
+  damage = 10;
+  flankingBonus = 2 * DEFAULT_FLANKING_BONUS;
 }
